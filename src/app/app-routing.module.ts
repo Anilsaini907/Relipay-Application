70ef694f273a67da7e1dd7fd93a45a20289d78a5
Sanjay Saini <sanjaysaini@Sanjays-MacBook-Air.local>
2020-09-08 23:41:55 +0530

    Relipay APPLICATION

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminpanelComponent } from './adminpanel/adminpanel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminpanelComponent },
+  { path: '',   redirectTo: '/admin', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminpanelComponent } from './adminpanel/adminpanel.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminpanelComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserAnimationsModule ,
+    AppRoutingModule,MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
